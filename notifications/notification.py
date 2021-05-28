4dda982ce4169cf357bc8f85a2e4186e4c237c98
Wroppy <wongw@scotscollege.school.nz>
2021-05-28 15:01:45 +1200

    Created the notification class for notifications

---
+++ notifications/notification.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+class Notification:
+    def __init__(self, title: str, message: str, year: int, month: int, day: int, hour: int, minute: int):
+        """
+        Constructor for the notifications
+
+        :param title: str
+        :param message: str
+        :param year: int
+        :param month: int
+        :param day: int
+        :param hour: int
+        :param minute: int
+        """
+
+        self.title = title
+        self.message = message
+
+        self.notificationTime = datetime(year, month, day, hour, minute)
