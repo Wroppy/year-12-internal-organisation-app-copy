8a3db0842cc5582d83d0553d86032085c0e2fd66
Wroppy <wongw@scotscollege.school.nz>
2021-06-09 21:31:21 +1200

    Coded the connect to database function

--- resourceManager/databaseHandler.py
+++ resourceManager/databaseHandler.py
@@ -1,6 +1,33 @@
+import pyodbc
+
+
 class CloudDataBase:
     def __init__(self):
         self.available = True
+        self.cursor = None
 
     def hasConnection(self):
         return self.available
+
+
+
+    def connectToDataBase(self) -> bool:
+        """
+        Attempts to connect to the database and returns a boolean variable
+
+        :returns: bool
+        """
+        try:
+            connection = pyodbc.connect(
+                'driver={SQL Server};  Server=3.25.137.79; Database=OrgApp; Trusted_Connection=no; UID=supermoon; PWD=Bluesky*99')
+
+            self.cursor = connection.cursor()
+
+            return True
+        except (pyodbc.OperationalError, pyodbc.Error) as e:
+            print(e)
+            return False
+
+
+if __name__ == '__main__':
+    print(CloudDataBase().connectToDataBase())
