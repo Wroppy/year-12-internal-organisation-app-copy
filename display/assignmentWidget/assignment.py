463c0e0f664b96caab15fef5e4066d8d0c77e520
Wroppy <wongw@scotscollege.school.nz>
2021-06-11 09:17:08 +1200

    Created the assignment class

---
+++ display/assignmentWidget/assignment.py
@@ -0,0 +1,10 @@
+"""
+This file defines the assignment class
+"""
+
+
+class Assignment:
+    def __init__(self, title: str, description: str, completed: bool):
+        self.title = title
+        self.description = description
+        self.completed = completed
