283121c41b8b302c111805a377e0786c632158ed
Wroppy <wongw@scotscollege.school.nz>
2021-06-13 22:30:24 +1200

    Commented

--- passwordManager/accountSystem.py
+++ passwordManager/accountSystem.py
@@ -1,5 +1,9 @@
 import hashlib
 
 def hashPassword(password: str):
+    """
+    Hashes the password in sha256 encoding
+
+    """
     return hashlib.sha256(bytes(password, encoding="utf-8")).hexdigest()
 
